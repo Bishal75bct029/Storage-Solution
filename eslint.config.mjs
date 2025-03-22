6fb293dd0d24961965123d314c44dd1823993254
Bishal75bct029 <bishallc174@gmail.com>
2025-03-22 09:57:06 +0545

    set-up husky and prettier for code format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,31 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [".next/", "node_modules/"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      indent: ["error", 2],
+      "no-tabs": "error",
+      "no-trailing-spaces": "error",
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
